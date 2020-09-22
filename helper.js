ea0d87d19bd25f911aa7d7df81091f5fb8ba45cb
PraJha <abcpr108@gmail.com>
2020-09-22 19:11:40 +0530

    Now Code Readability is improved and also Back button is added in UI, but it's not functional as of now

--- helper.js
+++ helper.js
@@ -3,58 +3,59 @@ window.onload = function () {
     document.getElementById("save").onclick = function () {
         const key = document.getElementById("saveTitle").value
         const value = document.getElementById("saveDetails").value
-        
-        if(key.length !== 0 && value.length !== 0){
-            const jsonfile = {};
-            jsonfile[key] = value 
 
-            chrome.storage.local.set(jsonfile, function(){
-            })  
+        if (key.length !== 0 && value.length !== 0) {
+            const jsonfile = {}
+            jsonfile[key] = value
+
+            chrome.storage.local.set(jsonfile, function () {
+            })
         }
-        else{
+        else {
             return;
         }
     }
 
     document.getElementById("viewSavedItem").onclick = function () {
-        let data = ""
-        chrome.storage.local.get(null, function(items) {
-            keyArr = JSON.stringify(items);
-            data = JSON.parse(keyArr)
-            const obj1 = document.getElementById("submitForm"), obj2 = document.getElementById("showContent"), obj3 = document.getElementById("content")
-            
-            for (let key in data) {
-                let value = data[key]
-                let valueWithoutQuotes = value.replace(/['"]+/g, '')
-                let valueFinal = valueWithoutQuotes; 
-                console.log("valueFinal "+ valueFinal)
-                let h3 = document.createElement("H3"), br = document.createElement("BR")
-                let t3 = document.createTextNode(key)
-                let h4 = document.createElement("P")
-                let t4 = document.createTextNode(valueFinal)
-                let copyButton = document.createElement('button');
+        chrome.storage.local.get(null, function (items) {
+            keyValueJson = JSON.parse(JSON.stringify(items));
+            console.log("keyArr " + JSON.stringify(keyValueJson))
+            const obj1 = document.getElementById("submitForm")
+            const obj2 = document.getElementById("showContent")
+
+            for (let key in keyValueJson) {
+                const value = keyValueJson[key].replace(/['"]+/g, '')
+
+                const h3 = document.createElement("H3")
+                const t3 = document.createTextNode(key)
+                const h4 = document.createElement("P")
+                const t4 = document.createTextNode(value)
+                const copyButton = document.createElement('button');
+
                 h3.setAttribute("class", "contentTitle")
                 h4.setAttribute("class", "contentDetails")
                 copyButton.setAttribute("class", "copyButton")
-                copyButton.innerHTML = 'Copy';
-                copyButton.onclick = function(){
-                    const el = document.createElement('textarea');
-                    el.value = valueFinal;
-                    document.body.appendChild(el);
-                    el.select();
-                    document.execCommand('copy');
-                    document.body.removeChild(el);
+                copyButton.innerHTML = "Copy";
+
+                copyButton.onclick = function () {
+                    const el = document.createElement("textarea")
+                    el.value = value
+                    document.body.appendChild(el)
+                    el.select()
+                    document.execCommand("copy")
+                    document.body.removeChild(el)
                 };
 
-                let deleteButton = document.createElement('button');
+                const deleteButton = document.createElement("button")
                 deleteButton.setAttribute("class", "deleteButton")
-                deleteButton.innerHTML = 'Delete';
-                deleteButton.onclick = function(){
+                deleteButton.innerHTML = "Delete";
+
+                deleteButton.onclick = function () {
                     chrome.storage.local.remove(key)
                     h3.style.display = "none"
                     h4.style.display = "none"
                 };
-                
+
                 h3.appendChild(t3)
                 h3.appendChild(copyButton)
                 h3.appendChild(deleteButton)
